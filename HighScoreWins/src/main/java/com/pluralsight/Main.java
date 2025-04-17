ca2a015e5e046761f4eaf1424845c09684937661
Anna Harbour <101281738+annaharbour@users.noreply.github.com>
2025-04-17 15:29:30 -0400

    high score wins

--- HighScoreWins/src/main/java/com/pluralsight/Main.java
+++ HighScoreWins/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,35 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("Please enter a game score: ");
+        Scanner scanner = new Scanner(System.in);
+        String score = scanner.nextLine();
+        String[] scoreArr = score.split("[|:]");
+        String firstTeam = scoreArr[0];
+        String secondTeam = scoreArr[1];
+        int firstTeamScore = Integer.parseInt(scoreArr[2]);
+        int secondTeamScore = Integer.parseInt(scoreArr[3]);
+        String winner;
+        if (firstTeamScore > secondTeamScore) {
+            winner = firstTeam;
+        } else if (secondTeamScore > firstTeamScore){
+            winner = secondTeam;
+        } else {
+            winner = "Tie!";
+        }
+        System.out.println("Winner: " + winner);
     }
 }
+
+
+//- one split will be on the pipe ( | )
+//- one split will be on the colon ( : )
+//
+// ---
+//Please enter a game score: Home:Visitor|21:9↵
+//
+//Winner: Home
